26945b2fb4a7f1ffd90629c2793807644b5dfc17
Hernán Guillermo Dulcey Morán <guille.dulcey@gmail.com>
2019-10-22 17:59:11 -0500

    changing the name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/viajandopormexico'));
+app.use(express.static(__dirname + '/dist/viajando-por-mexico'));
 
 app.get('/*', function(req,res) {
-    const index = path.join(__dirname + '/dist/viajandopormexico/index.html');
+    const index = path.join(__dirname + '/dist/viajando-por-mexico/index.html');
     console.log(index);
     res.sendFile(index);
 });
