8b8b1ebc82d6c606a56ca0fa0f374d2246b32f9c
Hernán Guillermo Dulcey Morán <guille.dulcey@gmail.com>
2019-10-22 18:05:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/viajando-por-mexico'));
+app.use(express.static(__dirname + '/dist/ViajandoPorMexico'));
 
 app.get('/*', function(req,res) {
-    const index = path.join(__dirname + '/dist/viajando-por-mexico/index.html');
+    const index = path.join(__dirname + '/dist/ViajandoPorMexico/index.html');
     console.log(index);
     res.sendFile(index);
 });
