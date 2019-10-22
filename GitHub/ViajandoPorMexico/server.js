5c5f77ef12f06d38547e47754b4d8fe715554838
Hernán Guillermo Dulcey Morán <guille.dulcey@gmail.com>
2019-10-22 16:09:34 -0500

    changing deployment

--- GitHub/ViajandoPorMexico/server.js
+++ GitHub/ViajandoPorMexico/server.js
@@ -15,4 +15,4 @@ app.get('/*', function(req,res) {
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
-console.log(_dirname + '/dist/viajandopormexico/index.html');
+console.log(__dirname + '/dist/viajandopormexico/index.html');
