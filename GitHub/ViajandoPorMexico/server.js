769235035c4667e6cf1bb0410200687341a80b2d
Hernán Guillermo Dulcey Morán <guille.dulcey@gmail.com>
2019-10-22 15:18:52 -0500

    changing deployment route

--- GitHub/ViajandoPorMexico/server.js
+++ GitHub/ViajandoPorMexico/server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/viajandopormexico'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/viajandopormexico/index.html'));
+res.sendFile(path.join(__dirname,'./dist/viajandopormexico/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
